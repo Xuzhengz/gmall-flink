5f989139bd5c583681d76bce0cbe2c339fc1fa1a
Xuzhengz <2350122040@qq.com>
2022-11-29 19:58:46 +0800

    dws-2022-11-29

--- gmall-realtime/src/main/java/com/xzz/utils/PhoneixUtil.java
+++ gmall-realtime/src/main/java/com/xzz/utils/PhoneixUtil.java
@@ -6,7 +6,9 @@ import org.apache.commons.lang.StringUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -30,4 +32,5 @@ public class PhoneixUtil {
 //        4、释放连接
         preparedStatement.close();
     }
+
 }
